d7a5f736a471aa89f0ac6d4ad5b9b4507b6403a1
Juma Anton <47322942+Jumaantony@users.noreply.github.com>
2020-09-26 16:33:36 -0700

    Created an authentication system

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 def user_login(request):
     if request.method == 'POST':
@@ -11,12 +12,12 @@ def user_login(request):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
-                               username = cd['username'],
-                               password = cd['password'])
+                                username=cd['username'],
+                                password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated '\
+                    return HttpResponse('Authenticated '
                                         'successfully')
                 else:
                     return HttpResponse('Disabled account')
@@ -24,11 +25,11 @@ def user_login(request):
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render (request, 'account/login.html', {'form': form})
+    return render(request, 'account/login.html', {'form': form})
 
 
 @login_required
 def dashboard(request):
-    return render(request, 
-                    'account/dashboard.html', 
-                    {'section': 'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
